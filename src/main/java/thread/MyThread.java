88b3cd45e70d464755d8eb715e654242cf55c8e7
fupeng <fupeng@ppdai.com>
2020-06-23 13:53:58 +0800

    多线程

---
+++ src/main/java/thread/MyThread.java
@@ -0,0 +1,48 @@
+package thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author: fupeng
+ * @date: 2020/6/16 20:20
+ */
+class MyThread extends Thread {
+
+    public static void main(String[] args) {
+        try {
+            MyThread.test1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * lockInterruptibly 相应中断
+     * @throws Exception
+     */
+    public static void test1() throws Exception {
+        final Lock lock = new ReentrantLock();
+        lock.lock();
+        Thread.sleep(1000);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                lock.lock();
+	        	try {
+					lock.lockInterruptibly();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                System.out.println(Thread.currentThread().getName() + " interrupted.");
+            }
+        });
+        t1.start();
+        Thread.sleep(1000);
+        t1.interrupt();
+        Thread.sleep(1000000);
+    }
+
+}
