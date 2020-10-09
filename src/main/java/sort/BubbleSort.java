1e4db6839401f5eec9075bb0118fdacf4d7354cb
fupeng <fupeng@ppdai.com>
2020-10-09 19:32:39 +0800

    优化注释

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -17,24 +17,30 @@ public class BubbleSort {
      */
     public static void bubbleSort(int[] array) {
         int len = array.length - 1;
-        int temp; // 开辟一个临时空间, 存放交换的中间值
-        int tempPostion = 0;  // 记录最后一次交换的位置
+        // 开辟一个临时空间, 存放交换的中间值
+        int temp;
+        // 记录最后一次交换的位置
+        int tempPostion = 0;
         // 要遍历的次数
         for (int i = 0; i < array.length - 1; i++) {
             int flag = 1; // 设置一个标志位
-            // 依次的比较相邻两个数的大小，遍历一次后，把数组中第i小的数放在第i个位置上
+            // 依次比较相邻两个数的大小，遍历一次后，会将前面没有排好序的最大值放到后面位置
             for (int j = 0; j < len; j++) {
                 // 比较相邻的元素，如果前面的数大于后面的数，交换
                 if (array[j] > array[j + 1]) {
                     temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
-                    flag = 0;  // 发生交换，标志位置0
-                    tempPostion = j;  // 记录交换的位置
+                    // 发生交换，标志位置0
+                    flag = 0;
+                    // 记录交换的位置
+                    tempPostion = j;
                 }
             }
-            len = tempPostion; // 把最后一次交换的位置给len，来缩减内循环的次数
-            if (flag == 1) {// 如果没有交换过元素，则已经有序
+            // 把最后一次交换的位置给len，来缩减内循环的次数
+            len = tempPostion;
+            // 如果没有交换过元素，则已经有序
+            if (flag == 1) {
                 System.out.println(Arrays.toString(array));
                 return;
             }
